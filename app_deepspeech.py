f8ac74012b51067e8974456ff3fae9c9a401795f
wakepon21 <wkpn@outlook.jp>
2023-02-14 21:01:22 +0900

    add whisper

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -180,13 +180,13 @@ def app_sst(model_path: str, lm_path: str, lm_alpha: float, lm_beta: float, beam
 
 
                 #torch_audio = torch.from_numpy(np.frombuffer(buffer, np.int16).flatten().astype(np.float32) / 32768.0)
-                
-                torch_audio = whisper.pad_or_trim(buffer)
+                np_audio = np.frombuffer(buffer, np.float32)
+                np_audio = whisper.pad_or_trim(np_audio)
 #                mel = whisper.log_mel_spectrogram(torch_audio).to(transcriber.device)
 #                _, probs = transcriber.detect_language(mel)
 #                options = whisper.DecodingOptions(fp16 = False)
 #                result = whisper.decode(transcriber, mel, options)
-                torch_text = transcriber.transcribe(torch_audio, verbose=True, fp16=False,language='japanese')
+                torch_text = transcriber.transcribe(np_audio, verbose=True, fp16=False,language='japanese')
 #                text_output.markdown(f"**audio:** {torch_text.text},**Text:** {text}")
                 text_output.markdown(f"**audio:** {torch_text['text']} deep:{text}")
 
